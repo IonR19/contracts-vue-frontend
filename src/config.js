39ec6d7193d86376780c0a26f6f7b40d28517936
r19 <r19>
2021-03-16 16:12:04 +0300

    added env, new favicon

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://172.20.99.195:5000'
+// export const API_URL = process.env.ip || 'http://172.20.99.195:5000'
+const { VUE_APP_IP: ip, VUE_APP_PORT: port } = process.env;
+export const API_URL = `http://${ip}:${port}`
